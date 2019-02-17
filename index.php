d62e5e64b18793ff560813f8733e9a5f015d9b5f
myriam mansouri <myriam.mansouri@gmail.com>
2019-02-16 21:27:36 -0500

    initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <form class="contact-form" action="contactForm.php" method="post">
+ <input type="text" name="name" placeholder = "Name" />
+ <input type="text" name="email" placeholder = "Email" />
+ <input type="text" name="message" placeholder = "Message" />
+ <textarea name="message" placeholder = "Message"></textarea>
+ <p><input type="submit" /></p>
+</form>
+ <?php echo $_SERVER['HTTP_USER_AGENT'];
+ if (strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE') == FALSE) {
+    echo '<br />You are using Internet Explorer.<br />';
+}
+ ?> 
+ </body>
+</html>
