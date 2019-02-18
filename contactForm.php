ac9a824aad19215bcabece72883938651c398396
myriam mansouri <myriam.mansouri@gmail.com>
2019-02-18 10:44:28 -0500

    bug in contactForm php

--- contactForm.php
+++ contactForm.php
@@ -6,7 +6,7 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
 /* Include the Composer generated autoload.php file. */
-require dirname(__DIR__).'\vendor\autoload.php';
+require 'vendor/autoload.php';
 
 /* If you installed PHPMailer without Composer do this instead: */
 /*
