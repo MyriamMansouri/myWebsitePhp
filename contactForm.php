1a684fd4fb96c59f9f732267fb1573d9a43b961c
myriam mansouri <myriam.mansouri@gmail.com>
2019-02-18 10:22:50 -0500

    add phpmailer

--- contactForm.php
+++ contactForm.php
@@ -1,16 +1,71 @@
 <?php 
-    if (isset($_POST['submit'])) {
-        $name = $_POST['name'];
-        $mailFrom = $_POST['email'];
-        $message = $_POST['message'];
 
-        $mailTo = "info@myriammansouri.com";
-        $subject = 'New email from myriammansouri.com';
-        $headers = "From: ".$mailFrom;
-        $txt = "You have received an email from ".$name.".\n\n".$message;
-        $txt = str_replace("\n.", "\n..", $txt);
-        
-        mail($mailTo, $subject, $txt, $headers);
-        header("Location: indexphp?mailsent");
 
-    }
+/* Namespace alias. */
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+/* Include the Composer generated autoload.php file. */
+require 'C:\xampp\composer\vendor\autoload.php';
+
+/* If you installed PHPMailer without Composer do this instead: */
+/*
+require 'C:\PHPMailer\src\Exception.php';
+require 'C:\PHPMailer\src\PHPMailer.php';
+require 'C:\PHPMailer\src\SMTP.php';
+*/
+
+/* Create a new PHPMailer object. Passing TRUE to the constructor enables exceptions. */
+$mail = new PHPMailer(TRUE);
+
+$name = $_POST['name'];
+$mailFrom = $_POST['email'];
+$message = $_POST['message'];
+
+$mailTo = "info@myriammansouri.com";
+$subject = 'New email from myriammansouri.com';
+$headers = "From: ".$mailFrom;
+$txt = "You have received an email from ".$name.".\n\n".$message;
+$txt = str_replace("\n.", "\n..", $txt);
+
+if (isset($_POST['submit'])) {
+/* Open the try/catch block. */
+try {
+   /* Set the mail sender. */
+   $mail->setFrom($mailFrom, $name);
+
+   /* Add a recipient. */
+   $mail->addAddress($mailTo);
+
+   /* Set the subject. */
+   $mail->Subject = $subject;
+
+   /* Set the mail message body. */
+   $mail->Body = $txt;
+
+   $mail->isSMTP();
+   $mail->Host = 'smtp.gmail.com';
+   $mail->SMTPAuth = TRUE;
+   $mail->SMTPSecure = 'tls';
+   $mail->Username = $mailFrom;
+   $mail->Password = 'Psylormyriam0+';
+   $mail->Port = 587;
+   
+   /* Enable SMTP debug output. */
+   $mail->SMTPDebug = 4;
+
+   /* Finally send the mail. */
+   $mail->send();
+}
+catch (Exception $e)
+{
+   /* PHPMailer exception. */
+   echo $e->errorMessage();
+}
+catch (\Exception $e)
+{
+   /* PHP exception (note the backslash to select the global namespace Exception class). */
+   echo $e->getMessage();
+}
+
+}
