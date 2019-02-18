62864b2dd3328da99b709b6cd9e60371fdfbfbf4
myriam mansouri <myriam.mansouri@gmail.com>
2019-02-18 14:01:03 -0500

    loaded to prod server for testing

--- index.php
+++ index.php
@@ -65,16 +65,11 @@ catch (\Exception $e)
   <title>PHP Test</title>
  </head>
  <body>
- <form class="contact-form" action="contactForm.php" method="post">
+ <form class="contact-form" method="post">
  <input type="text" name="name" placeholder = "Name" />
  <input type="text" name="email" placeholder = "Email" />
  <textarea name="message" placeholder = "Message"></textarea>
  <p><input type="submit" /></p>
 </form>
- <?php echo $_SERVER['HTTP_USER_AGENT'];
- if (strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE') == FALSE) {
-    echo '<br />You are using Internet Explorer.<br />';
-}
- ?> 
  </body>
 </html>
